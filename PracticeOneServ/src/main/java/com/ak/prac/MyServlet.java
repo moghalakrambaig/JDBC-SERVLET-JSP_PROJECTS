d649c4780d69420d81927c937c41135427a05f6e
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-03-20 08:02:51 +0530

    First commit for adv.java

---
+++ PracticeOneServ/src/main/java/com/ak/prac/MyServlet.java
@@ -0,0 +1,15 @@
+package com.ak.prac;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("HEllooo");
+	}
+}
